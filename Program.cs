e1aec2bae9f11f37b3d60ba5d434b9947cebcc2e
Yunus Yeşilçimen <135322317+esbyunus@users.noreply.github.com>
2024-02-28 23:31:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                Console.WriteLine("Kullanci adiniz ?: ");
+                string kullanciadi = Console.ReadLine();
+
+                Console.WriteLine("Sifreniz ?: ");
+                string sifre = Console.ReadLine();
+
+                if (kullanciadi != "user")
+                {
+                    Console.WriteLine("Kullanici adiniz hatali!");
+                }
+                else if (sifre != "asd123")
+                {
+                    Console.WriteLine("Sifreniz hatali!");
+                }
+                else
+                {
+                    Console.WriteLine("Basarili giris yaptiniz");
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
